365b320de890695e0aaea030b0bcfd200689916c
David <G00420905@atu.ie>
2025-10-22 16:37:11 +0100

    Try and catch feature (done part 1)

--- src/main/java/ie/atu/TryMain.java
+++ src/main/java/ie/atu/TryMain.java
@@ -1,20 +1,25 @@
 package ie.atu;
 import java.util.Scanner;
 
-public class TryMain {
-    public static void main(String[] args) {
+public class TryMain
+{
+    public static void main(String[] args)
+    {
         Scanner sc=new Scanner(System.in);
         int age;
-        System.out.print("Enter your age: ");
-        String text = sc.nextLine().trim();
-        try{
-            age=Integer.parseInt(text);
-        }
-        catch(NumberFormatException e)
+        while(true)
         {
-            System.out.println("Invalid input" + e.getMessage());
+            System.out.print("Enter your age: ");
+            String text = sc.nextLine().trim();
+            try{
+                age=Integer.parseInt(text);
+                System.out.println("Thanks!\nYour age is " + age);
+                break;
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Invalid input " + e.getMessage());
+            }
         }
     }
-
-
 }
